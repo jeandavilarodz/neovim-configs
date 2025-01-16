22d97125d324f6d5fcd993e052b7caaabd9bedef
jeandavilarodz <33385948+jeandavilarodz@users.noreply.github.com>
2025-01-15 22:11:23 -0500

    Making tabs 4 spaces

--- init.lua
+++ init.lua
@@ -8,3 +8,9 @@ require("config.lazy")
 
 -- Colorscheme
 vim.cmd.colorscheme(require("config.colorscheme").name)
+
+-- Set tabs to 4 spaces
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
