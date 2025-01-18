fde2136019f80dd6b31071726a1bc3dee2896a82
jeandavilarodz <33385948+jeandavilarodz@users.noreply.github.com>
2025-01-17 22:07:01 -0500

    Changing icon provider

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,6 @@
 return {
   'stevearc/oil.nvim',
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
       require("oil").setup {
           columns = { "icon" },
